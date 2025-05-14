4561e93e41ac97787f806e44683f83a86c95e904
Bonga <bongamzongwana@gmail.com>
2025-05-14 15:50:27 +0200

    created the appbar, chats, status, calls tab. created and populated the contact list

---
+++ whatsapp_clone/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/color.dart';
+import 'package:whatsapp_clone/responsive/responsive_layout.dart';
+import 'package:whatsapp_clone/screens/mobile_screen_layout.dart';
+import 'package:whatsapp_clone/screens/web_screen_layout.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Whatsapp',
+      theme: ThemeData.dark().copyWith(
+          colorScheme:
+              ThemeData.dark().colorScheme.copyWith(surface: backgroundColor)),
+      home: const Responsivelayout(
+          mobileScreenLayout: MobileScreenLayout(),
+          webScreenLayout: WebScreenLayout()),
+    );
+  }
+}
---
+++ whatsapp_clone/lib/screens/mobile_screen_layout.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/color.dart';
+import 'package:whatsapp_clone/widgets/contacts_list.dart';
+
+class MobileScreenLayout extends StatelessWidget {
+  const MobileScreenLayout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: appBarColor,
+          elevation: 0,
+          title: const Text(
+            'WhatsApp',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          centerTitle: false,
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.search, color: Colors.white),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.more_vert,
+                color: Colors.white,
+              ),
+            ),
+          ],
+          bottom: const TabBar(
+              labelColor: tabColor,
+              unselectedLabelColor: Colors.white,
+              indicatorColor: tabColor,
+              tabs: [
+                Tab(
+                  text: 'chats',
+                ),
+                Tab(
+                  text: 'status',
+                ),
+                Tab(
+                  text: 'calls',
+                )
+              ]),
+        ),
+        body: const ContactsList(),
+      ),
+    );
+  }
+}
