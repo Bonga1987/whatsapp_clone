402fe478876d75b71bde2cf00fb7463620ef4979
Bonga <bongamzongwana@gmail.com>
2025-05-15 15:46:55 +0200

    created and displayed web profile bar and search profile bar

--- whatsapp_clone/lib/screens/web_screen_layout.dart
+++ whatsapp_clone/lib/screens/web_screen_layout.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:whatsapp_clone/widgets/contacts_list.dart';
+import 'package:whatsapp_clone/widgets/web_profile_bar.dart';
+import 'package:whatsapp_clone/widgets/web_search_bar.dart';
 
 class WebScreenLayout extends StatelessWidget {
   const WebScreenLayout({super.key});
@@ -14,8 +16,8 @@ class WebScreenLayout extends StatelessWidget {
           child: SingleChildScrollView(
             child: Column(
               children: [
-                //web profile bar
-                //web search bar
+                WebProfileBar(),
+                WebSearchBar(),
                 ContactsList(),
               ],
             ),
---
+++ whatsapp_clone/lib/widgets/web_profile_bar.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/color.dart';
+
+class WebProfileBar extends StatelessWidget {
+  const WebProfileBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height * 0.077,
+      width: MediaQuery.of(context).size.width * 0.30,
+      padding: const EdgeInsets.all(10),
+      decoration: const BoxDecoration(
+        border: Border(
+          right: BorderSide(color: dividerColor),
+        ),
+        color: webAppBarColor,
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          const CircleAvatar(
+            backgroundImage: NetworkImage(
+                'https://media.cntraveler.com/photos/60596b398f4452dac88c59f8/16:9/w_3999,h_2249,c_limit/MtFuji-GettyImages-959111140.jpg'),
+            radius: 20,
+          ),
+          Row(
+            children: [
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.comment,
+                  color: Colors.grey,
+                ),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.more_vert,
+                  color: Colors.grey,
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
