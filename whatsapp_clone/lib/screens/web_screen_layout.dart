7b4af69ab53611c95bf6abac15df6ae9ca5862df
Bonga <bongamzongwana@gmail.com>
2025-05-15 14:47:50 +0200

    created web screen layout and displayed web contact list

--- whatsapp_clone/lib/screens/web_screen_layout.dart
+++ whatsapp_clone/lib/screens/web_screen_layout.dart
@@ -1,14 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/widgets/contacts_list.dart';
 
 class WebScreenLayout extends StatelessWidget {
   const WebScreenLayout({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('This is web'),
-      ),
-    );
+    return Scaffold(
+        body: Row(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        const Expanded(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                //web profile bar
+                //web search bar
+                ContactsList(),
+              ],
+            ),
+          ),
+        ),
+
+        //web screen
+        Container(
+            width: MediaQuery.of(context).size.width * 0.70,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage(
+                    "assets/backgroundImage.png",
+                  ),
+                  fit: BoxFit.cover),
+            ),
+            child: const Text('chats are here')),
+      ],
+    ));
   }
 }
